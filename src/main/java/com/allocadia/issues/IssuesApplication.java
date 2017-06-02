2bd254a81d71949b2cb9be810806350f8f18f6c1
Josh <josh.barnes@allocadia.com>
2017-06-01 21:17:20 -0700

    Initial Commit

---
+++ src/main/java/com/allocadia/issues/IssuesApplication.java
@@ -0,0 +1,12 @@
+package com.allocadia.issues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesApplication.class, args);
+	}
+}
